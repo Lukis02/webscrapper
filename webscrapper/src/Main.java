ab0dcd873f30a5d4a6f879c7c41fe5be4f73f115
Lukis02 <74176312+Lukis02@users.noreply.github.com>
2021-01-29 14:09:28 +0100

    Add files via upload

--- webscrapper/src/Main.java
+++ webscrapper/src/Main.java
@@ -3,17 +3,33 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import javax.sound.midi.SysexMessage;
-import java.util.List;
+
 import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+        amazon();
+        Scanner sc = new Scanner(System.in);
+
+
+
+        pccomponentes();
+
+
+        // Obtener cada uno de los artículos
+
+
+
+
+
+
+    }
     private static ChromeDriver driver = null;
 
 
-    public static void Chrome() {
+    public static void amazon() {
 
         String exePath = "C:\\Users\\user02\\Downloads\\chromedriver_win32\\chromedriver.exe";
         System.setProperty("webdriver.chrome.driver", exePath);
@@ -21,31 +37,65 @@ public class Main {
         options.addArguments("--start-maximized");
         driver = new ChromeDriver(options);
         driver.get("https://www.amazon.es/gp/browse.html?node=931491031&ref_=nav_em__tele_0_2_13_3");
-    }
 
-    public static void main(String[] args) {
-        Chrome();
         Scanner sc = new Scanner(System.in);
-        System.out.println("Introduce el producto que desee:");
+        System.out.println("Introduzca el producto que desee:");
         String Busqueda = sc.nextLine();
         WebElement cajaBusqueda =
                 driver.findElement(By.id("twotabsearchtextbox"));
         cajaBusqueda.sendKeys(Busqueda);
         cajaBusqueda.submit();
 
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
-        // Obtener cada uno de los artículos
 
-        int j=1;
+        for (int i = 2; i < 7; i++) {
+                try {
+                    String modelo = driver.findElement(By.xpath("/html/body/div[2]/div[1]/div[4]/ul/li["+i+"]/div/div/div[1]/div[2]/div[1]/div[2]/div[2]/div[1]/span[1]/span[1]")).getText();
+                    String precio = driver.findElement(By.xpath("/html/body/div[2]/div[1]/div[4]/ul/li["+i+"]/div/div/div[1]/div[2]/div[1]/div[2]/div[2]/div[1]/span[1]/span[1]")).getText();
+                System.out.println(modelo);
+                System.out.println(precio);
+        }catch (Exception e) {
 
-        for (int i=4; i<5;i++)
-        {
-            String modelo = driver.findElement(By.xpath("/html/body/div[1]/div[2]/div[1]/div[2]/div/span[3]/div[2]/div["+i+"]/div/span/div/div/div[2]/div[2]/div/div[2]/div[1]/div/div[1]/div/div/div/a/span/span[2]/span[1]")).getText();
-        String precio = driver.findElement(By.xpath("/html/body/div[1]/div[2]/div[1]/div[2]/div/span[3]/div[2]/div["+i+"]/div/span/div/div/div[2]/div[2]/div/div[2]/div[1]/div/div[1]/div/div/div/a/span/span[2]/span[1]")).getText();
-            System.out.println(modelo);
-            System.out.println(precio);
+                }
+
+    }}
+
+    public static void pccomponentes() {
+        String exePath = "C:\\Users\\user02\\Downloads\\chromedriver_win32\\chromedriver.exe";
+        System.setProperty("webdriver.chrome.driver", exePath);
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        driver = new ChromeDriver(options);
+        driver.get("https://www.pccomponentes.com/smartphone-moviles");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduzca el producto que desee:");
+        String Busqueda = sc.nextLine();
+        WebElement cajaBusqueda =
+                driver.findElement(By.name("query"));
+        cajaBusqueda.sendKeys(Busqueda);
+        cajaBusqueda.submit();
+
+
+        for (int i = 2; i < 6; i++) {
+
+            try {
+
+
+                String modelo1 = driver.findElement(By.xpath("/html/body/div[3]/div[2]/div/div/div[2]/div[2]/div[3]/div/div[" + i + "]/article/div/div/header/h3/a")).getText();
+                String precio1 = driver.findElement(By.xpath("/html/body/div[3]/div[2]/div/div/div[2]/div[2]/div[3]/div/div[" + i + "]/article/div/div/div[2]/div/span")).getText();
+                System.out.println(modelo1);
+                System.out.println(precio1 + "€");
+            } catch (Exception e) {
+
+            }
+
         }
     }
+
 }
-
-
